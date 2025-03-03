cae0c0bab57403a3f76e900ee73339993350a7de
FA <fabricecoder009@gmail.com>
2025-03-03 14:00:19 +0200

    feat: added user dao implementer

---
+++ src/main/java/com/example/bookstore/services/IBookService.java
@@ -0,0 +1,12 @@
+package com.example.bookstore.services;
+
+import com.example.bookstore.models.Book;
+import java.util.List;
+
+public interface IBookService {
+    List<Book> getAllBooks();
+    Book getBookById(int id);
+    boolean addBook(Book book);
+    boolean updateBook(Book book);
+    boolean deleteBook(int id);
+}
--- src/main/java/com/example/bookstore/utils/DbConnection.java
+++ src/main/java/com/example/bookstore/utils/DbConnection.java
@@ -1,9 +1,21 @@
 package com.example.bookstore.utils;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DbConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/online_bookstore";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "root";
+
     public static Connection getConnection() {
-        System.out.println("Connecting to database...");
+        try {
+            Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database connection failed.");
+        }
     }
 }
