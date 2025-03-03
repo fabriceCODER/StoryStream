afd809759875b90d3b87a8837159b9e71f3dd793
FA <fabricecoder009@gmail.com>
2025-03-03 14:02:49 +0200

    feat: added book service implementer

---
+++ src/main/java/com/example/bookstore/servicesImpl/BookServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.bookstore.servicesImpl;
+
+import com.example.bookstore.dao.IBookDAO;
+import com.example.bookstore.daoImpl.BookDAOImpl;
+import com.example.bookstore.models.Book;
+import com.example.bookstore.services.IBookService;
+import java.util.List;
+
+public class BookServiceImpl implements IBookService {
+    private final IBookDAO bookDAO = new BookDAOImpl();
+
+    @Override
+    public List<Book> getAllBooks() {
+        return bookDAO.getAllBooks();
+    }
+
+    @Override
+    public Book getBookById(int id) {
+        return bookDAO.getBookById(id);
+    }
+
+    @Override
+    public boolean addBook(Book book) {
+        return bookDAO.addBook(book);
+    }
+
+    @Override
+    public boolean updateBook(Book book) {
+        return bookDAO.updateBook(book);
+    }
+
+    @Override
+    public boolean deleteBook(int id) {
+        return bookDAO.deleteBook(id);
+    }
+}
